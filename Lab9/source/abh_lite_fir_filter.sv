418720a7cf3155773cfb1c20c40c2ab6ce6820e5
Timothy Hein <heint@purdue.edu>
2023-03-08 14:19:21 -0500

    got started

---
+++ Lab9/source/abh_lite_fir_filter.sv
@@ -0,0 +1,16 @@
+module(
+    input logic clk,
+    input logic n_rst,
+    input logic hsel,
+    input logic [3:0] haddr,
+    input logic hsize,
+    input logic htrans[1:0],
+    input logic hwrite,
+    input logic hwdata[15:0],
+    output logic hrdata[15:0],
+    output logic hresp
+);
+
+
+
+endmodule
---
+++ Lab9/source/coefficient_loader.sv
@@ -0,0 +1,10 @@
+module coefficientloader(
+    input logic clk,
+    input logic n_rst,
+    input logic new_coefficient_set,
+    input logic modwait,
+    output logic load_coeff,
+    output logic [1:0] coefficient_num
+);
+
+endmodule
